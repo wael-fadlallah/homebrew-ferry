57d0a041f29053590adee5c801cd858410218e52
wael-fadlallah <wael.fudlallah@gmail.com>
2025-07-14 01:51:05 +0400

    Update download URLs in Ferry formula to use version variable

--- Formula/ferry.rb
+++ Formula/ferry.rb
@@ -5,10 +5,10 @@ class Ferry < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/wael-fadlallah/ferry/releases/download/v1.0.0/ferry_Darwin_arm64.tar.gz"
+      url "https://github.com/wael-fadlallah/ferry/releases/download/v#{version}/ferry_Darwin_arm64.tar.gz"
       sha256 "57d668dcbeb8588ec531601d8f8c08ba64b76a6f6235c4edf11f3beda7635a44"
     else
-      url "https://github.com/wael-fadlallah/ferry/releases/download/v1.0.0/ferry_Darwin_x86_64.tar.gz"
+      url "https://github.com/wael-fadlallah/ferry/releases/download/v#{version}/ferry_Darwin_x86_64.tar.gz"
       sha256 "6dc4f0cc09ee894652ce3cab649e818a3b52854f2b0cf7a66086002164f37ffe"
     end
   end
